7b16b3288b0471427dded3c1811457dafee4b30f
Pralay <pralaydutta666.pd@gmail.com>
2024-10-13 19:45:49 +0530

    some changes

--- allStructures.h
+++ allStructures.h
@@ -6,14 +6,12 @@
 #include<fcntl.h>
 
 struct Customer {
-public:
     char userid[100];
     char password[10];
     int balance;
 };
 
 struct Transaction{
-public:
     int unique_id;
     char userid[100];
     char transaction_type[50];
@@ -22,24 +20,23 @@ public:
     int balance_after;
 
 };
-void update_history(struct Transaction trans){
-    FILE *file = fopen("transactions.txt", "r");
-    if (file == NULL) {
-        perror("Error opening file");
-        return;
-    }
-    int fd = fileno(file);
-    struct flock lock;
+// void update_history(struct Transaction trans){
+//     FILE *file = fopen("transactions.txt", "r");
+//     if (file == NULL) {
+//         perror("Error opening file");
+//         return;
+//     }
+//     int fd = fileno(file);
+//     struct flock lock;
 
-    fseek(file, 0, SEEK_SET);// Setting the head to where user block is
-    if (fread(&trans, sizeof(struct Customer), 1, file) != 1){
-        perror("Error reading user data");
-        return 1;
-    }
-}
+//     fseek(file, 0, SEEK_SET);// Setting the head to where user block is
+//     if (fread(&trans, sizeof(struct Customer), 1, file) != 1){
+//         perror("Error reading user data");
+//         return 1;
+//     }
+// }
 
 struct Employee{
-public:
     char userid[100];
     char password[10];
     int loanAppl[1024];
@@ -48,7 +45,6 @@ public:
 };
 
 struct Manager{
-public:
     char userid[100];
     char password[10];
 
