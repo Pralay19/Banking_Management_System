5ac70071cf03ce9dcda8ab80a2a83b91ec174646
Pralay <pralaydutta666.pd@gmail.com>
2024-10-16 11:46:08 +0530

    initialized server and client programs

---
+++ server2.c
@@ -0,0 +1,23 @@
+/* 
+====================================
+* PROGRAM: server2.c
+* AUTHOR: PRALAY D. SAW
+* ROLLNO: MT2024119
+* DESCRIPTION:
+
+====================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <stdbool.h>
+
+int main() {
+    int server_socket,client_socket,*new_sock;
+    
+}
